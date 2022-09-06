b5c977584bd9768bf771dd0de54363e659ae5fa6
president <abanda435@gmail.com>
2022-09-06 15:36:23 +0200

    fixed no render of the app component

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//import React from 'react';
+// import { ReactDOM } from 'react-dom';
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.render(<App />, document.querySelector('#root'))
+
+
+import React from "react"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import './index.css';
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
