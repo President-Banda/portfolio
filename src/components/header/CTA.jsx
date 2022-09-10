9621781e17ed8d3ea87375705b983b14c8400997
president <abanda435@gmail.com>
2022-09-10 22:50:03 +0200

    fixed cv download link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,8 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href="{CV}" download>Download CV</a>
+        {/* <a href="{CV}">Download CV</a> */}
+        <a href="../../assets/cv.pdf" download>Download CV</a>
         <a href="#contact">Let's Talk</a>
     </div>
   )
