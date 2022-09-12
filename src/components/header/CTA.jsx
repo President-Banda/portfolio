c8da4a55c97f65fc6689d9c70d89e4ea02fcfdf3
president <abanda435@gmail.com>
2022-09-12 11:30:49 +0200

    cv download not working, commented i out

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+//import CV from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
         {/* <a href="{CV}">Download CV</a> */}
-        <a href="../../assets/cv.pdf" download>Download CV</a>
+        <a href="../../assets/cv.pdf" download='Albert_Banda_cv.pdf'>Download CV</a> &nbsp;
         <a href="#contact">Let's Talk</a>
     </div>
   )
