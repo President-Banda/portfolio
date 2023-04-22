63dd8652c22a43e87abe5aee0161194a3708e849
president <abanda435@gmail.com>
2023-04-22 22:06:26 +0200

    added linkedin link without customization

--- app/links/page.jsx
+++ app/links/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const LinkComponent = () => {
@@ -16,7 +17,10 @@ const LinkComponent = () => {
       </div>
 
       <div className="col-span-4 text-justify text-center m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed bg-gradient-to-r from-sakura to-sakura-light h-60">
-        <h1 className='text-center text-4xl'>Connect with me via:</h1>Link Element 4
+        <h1 className='text-center text-4xl'>Connect with me via:</h1>
+        <Link href='http://www.linkedin.com/in/albert-banda-tech' target='blank'>
+          LinkedIn
+        </Link>
       </div>
 
     </>
