e28a489e4d2b272348354b1cc6b047227e47d9a8
president <abanda435@gmail.com>
2023-04-22 22:07:01 +0200

    Fixed link redirection to linkedin

--- app/links/page.jsx
+++ app/links/page.jsx
@@ -18,7 +18,7 @@ const LinkComponent = () => {
 
       <div className="col-span-4 text-justify text-center m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed bg-gradient-to-r from-sakura to-sakura-light h-60">
         <h1 className='text-center text-4xl'>Connect with me via:</h1>
-        <Link href='http://www.linkedin.com/in/albert-banda-tech' target='blank'>
+        <Link href='http://www.linkedin.com/in/albertbanda-tech' target='blank'>
           LinkedIn
         </Link>
       </div>
