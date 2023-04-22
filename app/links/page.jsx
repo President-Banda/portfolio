11f710f6bdb6c06ac3990a61ba25bc8a8a7e1006
president <abanda435@gmail.com>
2023-04-22 23:06:18 +0200

    added transition and social media icons to links

--- app/links/page.jsx
+++ app/links/page.jsx
@@ -2,6 +2,10 @@ import Link from 'next/link'
 import React from 'react'
 import { Linkedin } from '../../components/_icons/linkedin'
 import { Githubk } from '../../components/_icons/github'
+import { Twitter } from '../../components/_icons/twitter'
+import { Mail } from '../../components/_icons/mail'
+import { Tinder } from '../../components/_icons/tinder'
+
 
 const LinkComponent = () => {
   return (
@@ -22,35 +26,52 @@ const LinkComponent = () => {
         <h1 className='text-center text-4xl mb-10'>Connect with me via:</h1>
         <div className='text-center'>
             <Link href='http://www.linkedin.com/in/albertbanda-tech' target='blank'
-            className='font-semibold border border-solid  border-black rounded-md m-1 p-4 h-10 w-28 inline-flex text-lg text-center justify-center align-middle items-center'
+            className='font-semibold border border-solid  border-black rounded-md 
+            m-1 p-4 h-10 w-28 inline-flex text-lg 
+            text-center justify-center align-middle items-center 
+            hover:scale-150 transition'
             >
               <Linkedin className='mr-1 scale-150' />
               LinkedIn
             </Link>
 
             <Link href='https://github.com/President-Banda' target='blank'
-            className='font-semibold border border-solid  border-black rounded-md m-1 p-4 h-10 w-28 inline-flex text-lg text-center justify-center align-middle items-center'
+            className='font-semibold border border-solid  border-black rounded-md 
+            m-1 p-4 h-10 w-28 inline-flex text-lg 
+            text-center justify-center align-middle items-center
+            hover:scale-150 transition'
             >
               <Githubk className='mr-1 scale-150' />
               GitHub
             </Link>
 
             <Link href='https://twitter.com/president_banda' target='blank'
-            className='font-semibold border border-solid border-black rounded-md m-1 p-4 h-10 w-28 inline-flex text-lg text-center justify-center align-middle items-center'
+            className='font-semibold border border-solid border-black rounded-md 
+            m-1 p-4 h-10 w-28 inline-flex text-lg 
+            text-center justify-center align-middle items-center
+            hover:scale-150 transition'
             >
+              <Twitter className='mr-1 scale-150' />
               Twitter
             </Link>
 
             <Link href='#' target='blank'
-            className='font-semibold border border-solid  border-black rounded-md m-1 p-4 h-10 w-28 inline-flex text-lg text-center justify-center align-middle items-center'
-            >
+            className='font-semibold border border-solid  border-black rounded-md
+            m-1 p-4 h-10 w-28 inline-flex text-lg 
+            text-center justify-center align-middle items-center
+            hover:scale-150 transition'
+            > 
+              <Mail className='mr-1 scale-150' />
               Mail
             </Link>
 
             <Link href='http://www.linkedin.com/in/albertbanda-tech' target='blank'
-            className='transition font-semibold border border-solid  border-black 
-            rounded-md m-1 p-4 h-10 w-28 inline-flex text-lg text-center justify-center align-middle items-center animate-pulse'
+            className='transition font-semibold border border-solid  border-black rounded-md
+             m-1 p-4 h-10 w-28 inline-flex text-lg 
+             text-center justify-center align-middle items-center 
+             animate-pulse hover:scale-150 transition'
             >
+              <Tinder className='mr-1 scale-150 animate-ping' />
               Tinder
             </Link>
 
--- components/_icons/tinder.tsx
+++ components/_icons/tinder.tsx
@@ -1,5 +1,4 @@
-
-export function ArcticonsTinder(props: SVGProps<SVGSVGElement>) {
+export function Tinder(props: SVGProps<SVGSVGElement>) {
     return (
       <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M25.79 4.5C34.16 8.4 40.4 18.38 40.4 27.1a16.4 16.4 0 0 1-32.8 0c0-.43 0-.85.05-1.28c.57-11.15 8.26-14.57 8.26-14.57s.76 4 2.57 6.27c0 0 8-5.42 7.31-13Z"></path><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M18.48 17.52a13.44 13.44 0 0 0 4.94 4.25"></path></svg>
     )
