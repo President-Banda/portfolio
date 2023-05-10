6e9c5481a79ff5532524fe12b11d4557ef5d2544
president <abanda435@gmail.com>
2023-05-10 19:45:28 +0200

    added experience and hobbies to intro

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -5,16 +5,23 @@ export default function Intro(){
         <>
             <div className="col-span-2 text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed">
                 <p className="p-2">
-				Albert Banda is...
+				Albert Banda is... Things I cannnot express on my CV...
 				
-                First and foremost, I am a tech enthusiast, I love technology, I love tweaking, creating, I love advancements 
+                First and foremost, I am a tech enthusiast, I love technology, I love tweaking/customizing, creating, I love advancements 
                 in technology. I love when hardware makes small leaps and bounds, I love when software improves, from rugged, unusable
                 messes that were car infotainment systems to the smooth ones we have now. I love technology across a large spectrum 
                 [linux, gaming, hardware, web applications, programming [ PHP, Laravel, Javascript, HTML, CSS, React, Next.js, Tailwind, MySQL ], 
-                android, ios, cloud, database, containerization] so I am still learning and finding my footing, but I also mention technologies
-                I frequently play around with.
+                android, ios, cloud, database, containerization, orchestration, automation, scripting, version control, A.I, Robotics, IoT] so I am still 
+                learning and finding my footing, but I also mention technologies I frequently play around with!
                 <br />
-                I
+                I am a Linux System Administrator by heart, managing complex hosting infrastructure and I love and enjoy learning about Linux, the 
+                stress of system administration is much more bearable. I am growing, even as you read this.
+                I am a web developer by need, I need money, mostly self taught but I have built some interesting projects over the years like this one,
+                some big, some small... But I am a pretty competent developer.
+                I am an AWS Cloud specialist by passion, we all need something to impress two out of five people in the room and my chosen
+                passion is Cloud technology, AWS cloud to be specific, a never ending learning journey I have come to love and specialize in.
+                I am a teacher by blood, both parents are teachers, my mother teaches nursery school kids, my father teaches primary school kids, 
+                not that they wanted me to be one but I do love what they do so much i just started teaching about... drum roll ... technology!
                 </p>
             </div>
 
