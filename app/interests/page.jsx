c3db24f422e62636807123d04b2173d3d48a3108
president <abanda435@gmail.com>
2023-05-29 22:23:16 +0200

    added hobbies and interests

--- app/interests/page.jsx
+++ app/interests/page.jsx
@@ -3,36 +3,71 @@ import React from 'react'
 const Interests = () => {
   return (
     <>
-       <div className="col-span-1 row-span-3 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-2/4">
+       <div className="col-span-1 row-span-7 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-full">
         <div className='glassmorphic-container'>
-          First div 1
+          <div className='text-xl text-black font-bold text-center'>Intellectual</div>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Programming</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Automation</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Reading</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Research</li>
         </div>
       </div>
-      <div className="col-span-1 row-span-4 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-64">
+      <div className="col-span-1 row-span-6 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-full">
         <div className='glassmorphic-container'>
-          First div 2
+          <div className='text-xl text-black font-bold text-center'>Casual</div>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Movies</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Gaming</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Anime</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Music?</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Twitter</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Cleaning</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>SLEEP</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Tweaking</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Customization/Configuration</li>
         </div>
       </div>
-      <div className="col-span-1 row-span-3 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-24">
+      <div className="col-span-1 row-span-8 col-start-2 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-72">
         <div className='glassmorphic-container'>
-          First div 3
+          <div className='text-xl text-black font-bold text-center'>Physical</div>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Gym</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Volleyball</li>
+        </div>
+        <div className='glassmorphic-container'>
+          <li>Football</li>
         </div>
       </div>
-      <div className="col-span-1 row-span-3 col-start-2 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-28">
-        <div className='glassmorphic-container'>
-          First div 4
-        </div>
-      </div>
-      <div className="col-span-1 row-span-2 col-start-3 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-32">
-        <div className='glassmorphic-container'>
-          First div 5
-        </div>
-      </div>
-      <div className="col-span-1 row-span-2 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-36">
-        <div className='glassmorphic-container'>
-          First div 6
-        </div>
-      </div>
+    
+      
     </>
       
   )
