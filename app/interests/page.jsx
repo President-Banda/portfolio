a114c99133c9b92880ed2f4656bdd2ea50d4640b
president <abanda435@gmail.com>
2023-05-29 16:08:06 +0200

    initial design for the interests and hobbies

--- app/interests/page.jsx
+++ app/interests/page.jsx
@@ -3,23 +3,35 @@ import React from 'react'
 const Interests = () => {
   return (
     <>
-       <div className="col-span-1 row-span-3 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-16">
-        First div
+       <div className="col-span-1 row-span-3 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-2/4">
+        <div className='glassmorphic-container'>
+          First div 1
+        </div>
       </div>
-      <div className="col-span-1 row-span-4 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-20">
-        Second div
+      <div className="col-span-1 row-span-4 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-64">
+        <div className='glassmorphic-container'>
+          First div 2
+        </div>
       </div>
       <div className="col-span-1 row-span-3 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-24">
-        Third div
+        <div className='glassmorphic-container'>
+          First div 3
+        </div>
       </div>
       <div className="col-span-1 row-span-3 col-start-2 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-28">
-        Fourth div
+        <div className='glassmorphic-container'>
+          First div 4
+        </div>
       </div>
       <div className="col-span-1 row-span-2 col-start-3 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-32">
-        Fifth div
+        <div className='glassmorphic-container'>
+          First div 5
+        </div>
       </div>
       <div className="col-span-1 row-span-2 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-36">
-        Sixth div
+        <div className='glassmorphic-container'>
+          First div 6
+        </div>
       </div>
     </>
       
