27ccc63075737bc1866ce0de8a71e742ae88dd57
president <abanda435@gmail.com>
2023-06-02 16:35:54 +0200

    added timeline component

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -1,11 +1,68 @@
 import React from 'react';
-import VerticalTimeline from '/components/Timeline';
 
 const Education = () => {
   return (
     <>
-     <div className='h-96 row-span-6 col-span-1 w-2/4 mt-10'>
-      <VerticalTimeline />
+     <div className='col-span-2 mt-10'>
+
+
+      <ol className="border-l border-neutral-300 dark:border-neutral-500">
+  <li>
+    <div className="flex-start flex items-center pt-3">
+      <div
+        className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
+      <p className="text-sm text-neutral-500 dark:text-neutral-300">
+        01.07.2021
+      </p>
+    </div>
+    <div className="mb-6 ml-4 mt-2">
+      <h4 className="mb-1.5 text-xl font-semibold">Title of section 1</h4>
+      <p className="mb-3 text-neutral-500 dark:text-neutral-300">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        scelerisque diam non nisi semper, et elementum lorem ornare.
+        Maecenas placerat facilisis mollis. Duis sagittis ligula in
+        sodales vehicula.
+      </p>
+    </div>
+  </li>
+
+  <li>
+    <div className="flex-start flex items-center pt-2">
+      <div
+        className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
+      <p className="text-sm text-neutral-500 dark:text-neutral-300">
+        13.09.2021
+      </p>
+    </div>
+    <div className="mb-6 ml-4 mt-2">
+      <h4 className="mb-1.5 text-xl font-semibold">Title of section 2</h4>
+      <p className="mb-3 text-neutral-500 dark:text-neutral-300">
+        Libero expedita explicabo eius fugiat quia aspernatur autem
+        laudantium error architecto recusandae natus sapiente sit nam
+        eaque, consectetur porro molestiae ipsam an deleniti.
+      </p>
+    </div>
+  </li>
+
+  <li>
+    <div className="flex-start flex items-center pt-2">
+      <div
+        className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
+      <p className="text-sm text-neutral-500 dark:text-neutral-300">
+        25.11.2021
+      </p>
+    </div>
+    <div className="ml-4 mt-2 pb-5">
+      <h4 className="mb-1.5 text-xl font-semibold">Title of section 3</h4>
+      <p className="mb-3 text-neutral-500 dark:text-neutral-300">
+        Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit
+        natus! Eum corporis illum nihil officiis tempore. Excepturi illo
+        natus libero sit doloremque, laborum molestias rerum pariatur quam
+        ipsam necessitatibus incidunt, explicabo.
+      </p>
+    </div>
+  </li>
+</ol>
      </div>
       
     </>
