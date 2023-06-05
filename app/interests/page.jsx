5b7f3c2db79462d2b54d4e3e8c4dcf1483725ced
president <abanda435@gmail.com>
2023-06-05 22:52:22 +0200

    added scale and reformatted background for interests and hobbies

--- app/interests/page.jsx
+++ app/interests/page.jsx
@@ -3,66 +3,66 @@ import React from 'react'
 const Interests = () => {
   return (
     <>
-       <div className="col-span-1 row-span-7 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-full background-image-div-one">
-        <div className='glassmorphic-container blur-none'>
-          <div className='text-xl text-black font-bold text-center'>Intellectual</div>
+       <div className="col-span-1 row-span-7 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-full ">
+        <div className='glassmorphic-container background-image-div-one'>
+          <div className='text-2xl text-black font-bold text-center'>Intellectual</div>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Programming</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Automation</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Reading</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Research</li>
         </div>
       </div>
-      <div className="col-span-1 row-span-6 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-full background-image-div-three">
-        <div className='glassmorphic-container'>
-          <div className='text-xl text-black font-bold text-center'>Casual</div>
+      <div className="col-span-1 row-span-6 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-full ">
+        <div className='glassmorphic-container background-image-div-three'>
+          <div className='text-2xl text-black font-bold text-center'>Casual</div>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Movies</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Gaming</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Anime</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Music?</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Twitter</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Cleaning</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>SLEEP</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Tweaking</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Customization/Configuration</li>
         </div>
       </div>
-      <div className="col-span-1 row-span-8 col-start-2 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-72 background-image-div-two">
-        <div className='glassmorphic-container'>
-          <div className='text-xl text-black font-bold text-center'>Physical</div>
+      <div className="col-span-1 row-span-8 col-start-2 text-justify border border-solid border-sakura-light rounded-lg p-1 leading-relaxed h-72">
+        <div className='glassmorphic-container background-image-div-two'>
+          <div className='text-2xl text-black font-bold text-center'>Physical</div>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Gym</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Volleyball</li>
         </div>
-        <div className='glassmorphic-container'>
+        <div className='glassmorphic-container scale-90'>
           <li>Football</li>
         </div>
       </div>
