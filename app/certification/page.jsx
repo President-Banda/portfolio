a9f863eb96b07b86deeb1baaf77898fe26efa02a
president <abanda435@gmail.com>
2023-06-07 00:50:16 +0200

    changed icons to jsx due to errors in build process

--- app/certification/page.jsx
+++ app/certification/page.jsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 const Certification = () => {
   return (
     <>
-      <div className="grid grid-cols-2 gap-3 col-start-2 col-span-3 text-justify m-2 border border-solid border-sakura-light rounded-lg leading-relaxed p-4 font-['Roboto']">
+      <div className="grid grid-cols-2 gap-1 col-start-2 col-span-3 text-justify m-2 border border-solid border-sakura-light rounded-lg leading-relaxed p-4 font-['Roboto']">
         <div>
-          <Image className="transition duration-100 rounded-xl shadow-inner scale-90 origin-center hover:rotate-45 hover:origin-top "
+          <Image className="transition duration-100 rounded-sm shadow-inner scale-90 origin-center hover:rotate-45 hover:origin-top "
               src={"/images/albert_banda_image.jpg"}
               alt="my_projects_image"
               width={500}
@@ -16,10 +16,11 @@ const Certification = () => {
               (max-width: 1200px) 50vw,
               33vw"
           />
+          <span>Amazon Authorised Instructor</span>
         </div>
 
         <div>
-        <Image className="transition duration-100 rounded-xl shadow-inner scale-90 origin-center hover:rotate-45 hover:origin-top "
+          <Image className="transition duration-100 rounded-sm shadow-inner scale-90 origin-center hover:rotate-45 hover:origin-top "
               src={"/images/albert_banda_image.jpg"}
               alt="my_projects_image"
               width={500}
@@ -30,7 +31,40 @@ const Certification = () => {
               (max-width: 1200px) 50vw,
               33vw"
           />
+          <span>AWS Certified Solutions Architect Associate</span>
         </div>
+
+        <div>
+          <Image className="transition duration-100 rounded-sm shadow-inner scale-90 origin-center hover:rotate-45 hover:origin-top "
+              src={"/images/albert_banda_image.jpg"}
+              alt="my_projects_image"
+              width={500}
+              height={500}
+              loading="lazy"
+
+              sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+          />
+          <span>Udacity Cloud Developer NanoDegree</span>
+        </div>
+
+        <div>
+          <Image className="transition duration-100 rounded-sm shadow-inner scale-90 origin-center hover:rotate-45 hover:origin-top "
+              src={"/images/albert_banda_image.jpg"}
+              alt="my_projects_image"
+              width={500}
+              height={500}
+              loading="lazy"
+
+              sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+          />
+          <span>FreeCodeCamp Front End Development Libraries</span>
+        </div>
+
+
       </div>
     </>
   )
