e48f71bae58ede4dcc20038c337ae0470dc61a22
president <abanda435@gmail.com>
2023-06-07 00:04:23 +0200

    redesigned certification to use one central div

--- app/certification/page.jsx
+++ app/certification/page.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const Certification = () => {
   return (
     <>
-      <div className="flex col-start-2 text-justify m-2 border border-solid border-sakura-light rounded-lg leading-relaxed p-4 font-['Roboto']">
-        Test
-      </div>
-
-      <div className="flex col-start-3 text-justify m-2 border border-solid border-sakura-light rounded-lg leading-relaxed p-4 font-['Roboto']">
+      <div className="grid col-start-2 col-span-3 text-justify m-2 border border-solid border-sakura-light rounded-lg leading-relaxed p-4 font-['Roboto']">
         Test
       </div>
     </>
