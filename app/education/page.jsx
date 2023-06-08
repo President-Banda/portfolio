dbe00d7fa95631d96da7873d9ceea7f4f95ba28b
president <abanda435@gmail.com>
2023-06-08 11:39:54 +0200

    added template for education and experience

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -3,30 +3,35 @@ import React from 'react';
 const Education = () => {
   return (
     <>
-    <div className="grid grid-cols-4 grid-rows-6 col-span-3 text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed">
+    <div className="grid grid-cols-4 col-span-3 text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed">
 
-        <div className="flex col-start-1 text-lg border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container w-0 ">
-            <ol className="relative border-l border-gray-200 dark:border-gray-700 justify-end text-end">                   
-                <li className="mb-10 ml-6">
-                    <span class="absolute flex items-center justify-center w-6 h-6 bg-sakura rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                        <svg aria-hidden="true" className="w-3 h-3 text-red-700 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                    </span>
-                    <time time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"> 
-                        7th, 2021
-                    </time>
-                </li>
-                <li className="ml-6">
-                    <span className="absolute flex items-center justify-center w-6 h-6 bg-sakura rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                        <svg aria-hidden="true" class="w-3 h-3 text-red-700 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                    </span>
-                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                        December 2nd, 2021
-                    </time>
-                </li>
+        <div className="grid grid-cols-1 grid-rows-6 col-start-1 text-lg border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container w-0 ">
+            <ol className="relative border-l border-gray-200 dark:border-gray-700 justify-end text-end row-span-6">                   
+                <div>
+                    <li className="mb-10 ml-6">
+                        <span class="absolute flex items-center justify-center w-6 h-6 bg-sakura rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <svg aria-hidden="true" className="w-3 h-3 text-red-700 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        </span>
+                        <time time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"> 
+                            7th, 2021
+                        </time>
+                    </li>
+                </div>
+
+                <div className='row-start-4'>
+                    <li className="ml-6">
+                        <span className="absolute flex items-center justify-center w-6 h-6 bg-sakura rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <svg aria-hidden="true" class="w-3 h-3 text-red-700 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        </span>
+                        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                            December 2nd, 2021
+                        </time>
+                    </li>
+                </div>
             </ol>
         </div>
 
-        <div className="text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container">
+        <div className="text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container h-fit">
             <ol className="relative border-l border-gray-200 dark:border-gray-700 p-5 h-fit">      
 
                 <li class="mb-10">            
@@ -35,20 +40,6 @@ const Education = () => {
                     </h3>
                     <time className="block mb-2 text-sm font-normal leading-relaxed text-gray-400 dark:text-gray-500">
                         GIBT
-                    </time>
-                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 text-justify">
-                        AWS Trainer for German Institute of business and Technology focusing on AWS
-                        certifications (CCP, CSAA) and training including subtopics such as Linux, Database
-                        Automation and Programming etc.
-                    </p>
-                </li>
-
-                <li class="mb-10">            
-                    <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                        System Admin/ Web Developer 
-                    </h3>
-                    <time className="block mb-2 text-sm font-normal leading-relaxed text-gray-400 dark:text-gray-500">
-                        Chiweto
                     </time>
                     <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 text-justify">
                         AWS Trainer for German Institute of business and Technology focusing on AWS
@@ -70,16 +61,30 @@ const Education = () => {
                     </p>
                 </li>
 
+                <li class="mb-10">            
+                    <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                        System Admin/ Web Developer 
+                    </h3>
+                    <time className="block mb-2 text-sm font-normal leading-relaxed text-gray-400 dark:text-gray-500">
+                        Chiweto
+                    </time>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 text-justify">
+                        AWS Trainer for German Institute of business and Technology focusing on AWS
+                        certifications (CCP, CSAA) and training including subtopics such as Linux, Database
+                        Automation and Programming etc.
+                    </p>
+                </li>
+
             </ol>
 
         </div>
 
-        <div className="text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container">
+        <div className=" row-start-2 col-start-3 -mt-48 text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container h-fit">
             <ol className="relative border-l border-gray-200 dark:border-gray-700 p-5">      
 
                 <li class="mb-10 ml-6">            
                     <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                        BSc. Information Technologu
+                        BSc. Information Technology
                     </h3>
                     <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                         UNIMA
@@ -93,7 +98,7 @@ const Education = () => {
 
         </div>
 
-        <div className="text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container">
+        <div className="col-start-4 row-start-2 -mt-96 text-justify text-lg m-2 border border-solid border-sakura-light rounded-lg p-1 leading-relaxed glassmorphic-container h-fit">
             <ol className="relative border-l border-gray-200 dark:border-gray-700 p-5">      
 
                 <li class="mb-10 ml-6">            
