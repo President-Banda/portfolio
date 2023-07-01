eae69ee683fef3d75f3fef3970756b85e51865e8
president <abanda435@gmail.com>
2023-07-01 22:21:21 +0200

    initial design for blog page

---
+++ app/blog/head.jsx
@@ -0,0 +1,8 @@
+export default function Head(){
+    return(
+        <>
+            <title>blog</title>
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
--- app/blog/layout.jsx
+++ app/blog/layout.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
+const header = (
+    <header>
+        <div>
+            <h1>
+                Albert Bandas blog
+            </h1>
+            <h1>
+                Welcome to the Presidents blog
+            </h1>
+        </div>
+    </header>
+)
+
+const footer = (
+    <footer>
+        <div>
+            <p>
+                Developed by Albert Banda
+            </p>
+        </div>
+    </footer>
+)
+
 const BlogLayout = ({ children }) => {
+
+    
   return (
     <div>
-        I am blog
+        { header }
         { children }
+        { footer }
     </div>
   )
 }
