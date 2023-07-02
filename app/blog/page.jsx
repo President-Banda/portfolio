49da65b8b93a6062711433712ce0aa5ca46a0d92
president <abanda435@gmail.com>
2023-07-02 16:15:13 +0200

    added titles to individual pages and initial code for posts

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,41 +1,30 @@
 import React from 'react'
+import fs from 'fs';
 import BlogLayout from './layout'
 
-// function getPostsMetaData = () => (
-//   const folder = "_posts/";
-//   const files = fstat.readdirsync(folder);
-// )
+const getPostsMetaData = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+
+  return slugs;
+};
 
 const Blog = () => {
+  const postMetaData = getPostsMetaData();
+  const postPreviews = postMetaData.map((slugs)  => (
+    <div>
+      <h2>
+        {slugs}
+      </h2>
+    </div>
+  ));
 
   return (
-    <>
-      What is AWS?
-
-      AWS stands for Amazon Web Services. It is a suite of cloud computing services that runs on the same infrastructure that Amazon uses for its e-commerce website. AWS offers a broad range of services, including compute, storage, databases, analytics, networking, machine learning, and artificial intelligence.
-
-      Benefits of AWS
-
-      There are many benefits to using AWS, including:
-
-          Scalability: AWS is highly scalable, so you can easily add or remove resources as needed. This makes it a good choice for businesses that experience fluctuating traffic or need to be able to handle spikes in demand.
-          Cost-effectiveness: AWS is a cost-effective way to host your applications and data. You only pay for the resources you use, so you can save money by only provisioning the resources you need.
-          Security: AWS is a secure platform, with a variety of features to help protect your data. These features include encryption, access control, and intrusion detection.
-          Global reach: AWS has a global network of data centers, so you can deploy your applications and data to users all over the world. This can help you improve performance and reduce latency for your users.
-
-      Conclusion
-
-      AWS is a powerful cloud computing platform that offers a wide range of benefits. If you are looking for a scalable, cost-effective, and secure way to host your applications and data, then AWS is a good option to consider.
-
-      Here are some additional benefits of AWS:
-
-          Reliability: AWS is a reliable platform, with a 99.9% uptime SLA. This means that your applications and data are highly likely to be available when you need them.
-          Innovation: AWS is constantly innovating, adding new services and features. This means that you can be sure that you are using the latest and greatest cloud computing technology.
-          Support: AWS offers a comprehensive support program, with 24/7 support available. This means that you can get help quickly if you need it.
-
-      If you are interested in learning more about AWS, you can visit the AWS website or sign up for a free trial.
-    </>
-    
+    <div>
+      
+    </div>    
   )
 }
 
---
+++ app/interests/head.jsx
@@ -0,0 +1,8 @@
+export default function Head(){
+    return(
+        <>
+            <title>interests</title>
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
