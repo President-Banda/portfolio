28c748bc8a6a21b052dd9dda2683b9f685570daa
president <abanda435@gmail.com>
2023-07-10 22:47:46 +0200

    added content view from files

--- app/blog/posts/[slug]/page.jsx
+++ app/blog/posts/[slug]/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import fs from 'fs';
+
+const getPostContent = ( slug ) => {
+  console.log(slug);
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`
+  const content = fs.readFileSync(file, "utf8");
+  return content;
+}
 
 const page = ( {params} ) => {
     const slug = params.slug;
+    const content = getPostContent(slug);
   return (
-    <div>page : {slug }</div>
+    <div>
+      page : {slug }
+      <p>{content}</p>
+    </div>
+    
   )
 }
 
